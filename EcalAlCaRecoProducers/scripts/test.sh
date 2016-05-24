c830a3048afe5482a4ea749961a3498ae3af73fa
J-C-Wright <jack.charles.wright@cern.ch>
2016-05-24 13:42:57 +0200

    Adds a variable to set number of events

--- EcalAlCaRecoProducers/scripts/test.sh
+++ EcalAlCaRecoProducers/scripts/test.sh
@@ -7,6 +7,7 @@ echo "Script for sandbox validation on a small amount of events   "
 temp=/tmp/$USER/sandboxValidation
 mkdir $temp
 
+num=100
 
 dir="Electron-Run2016B"
 mkdir $temp/{Single,Double}$dir
@@ -38,13 +39,13 @@ mySingleERecoFilePath="$singleDir/myreco_RAW2DIGI_RECO.root"
 myDoubleEGRecoFilePath="$doubleDir/myreco_RAW2DIGI_RECO.root"
 if [ ! -f "$mySingleERecoFilePath" ]; then
     echo "$mySingleERecoFilePath doesn't exist"
-    cmsDriver.py myreco -s RAW2DIGI,RECO -n 100  --filein="file:$myRawSingleEFileLocal" --data --conditions=$GT --era=Run2_2016  --scenario=pp --processName=reRECO --customise=L1Trigger/Configuration/customiseReEmul.L1TEventSetupForHF1x1TPs --dirout=$singleDir
+    cmsDriver.py myreco -s RAW2DIGI,RECO -n $num  --filein="file:$myRawSingleEFileLocal" --data --conditions=$GT --era=Run2_2016  --scenario=pp --processName=reRECO --customise=L1Trigger/Configuration/customiseReEmul.L1TEventSetupForHF1x1TPs --dirout=$singleDir
 else
     echo "$mySingleERecoFilePath exists"
 fi
 if [ ! -f "$myDoubleEGRecoFilePath" ]; then
     echo "myDoubleEGRecoFilePath doesn't exist"
-    cmsDriver.py myreco -s RAW2DIGI,RECO -n 100  --filein="file:$myRawDoubleEGFileLocal" --data --conditions=$GT --era=Run2_2016  --scenario=pp --processName=reRECO --customise=L1Trigger/Configuration/customiseReEmul.L1TEventSetupForHF1x1TPs --dirout=$doubleDir
+    cmsDriver.py myreco -s RAW2DIGI,RECO -n $num  --filein="file:$myRawDoubleEGFileLocal" --data --conditions=$GT --era=Run2_2016  --scenario=pp --processName=reRECO --customise=L1Trigger/Configuration/customiseReEmul.L1TEventSetupForHF1x1TPs --dirout=$doubleDir
 else
     echo "$myDoubleEGRecoFilePath exists"
 fi
@@ -54,13 +55,13 @@ myUncalWFilePath="$singleDir/EcalUncalWElectron.root"
 myUncalZFilePath="$doubleDir/EcalUncalZElectron.root"
 if [ ! -f "$myUncalWFilePath" ]; then
     echo "$myUncalWFilePath doesn't exist"
-    cmsDriver.py myreco -s ALCA:EcalUncalWElectron -n 100 --data --filein="file:$mySingleERecoFilePath" --conditions=$GT --secondfilein="file:$myRawSingleEFile" --dirout=$singleDir --era=Run2_2016  --scenario=pp --processName=alcaRaw --customise=L1Trigger/Configuration/customiseReEmul.L1TEventSetupForHF1x1TPs 
+    cmsDriver.py myreco -s ALCA:EcalUncalWElectron -n $num --data --filein="file:$mySingleERecoFilePath" --conditions=$GT --secondfilein="file:$myRawSingleEFile" --dirout=$singleDir --era=Run2_2016  --scenario=pp --processName=alcaRaw --customise=L1Trigger/Configuration/customiseReEmul.L1TEventSetupForHF1x1TPs 
 else
     echo "$myUncalWFilePath exists"
 fi
 if [ ! -f "$myUncalZFilePath" ]; then
     echo "$myUncalZFilePath doesn't exist"
-    cmsDriver.py myreco -s ALCA:EcalUncalZElectron -n 100 --data --filein="file:$myDoubleEGRecoFilePath" --conditions=$GT --secondfilein="file:$myRawDoubleEGFile" --dirout=$doubleDir --era=Run2_2016  --scenario=pp --processName=alcaRaw --customise=L1Trigger/Configuration/customiseReEmul.L1TEventSetupForHF1x1TPs 
+    cmsDriver.py myreco -s ALCA:EcalUncalZElectron -n $num --data --filein="file:$myDoubleEGRecoFilePath" --conditions=$GT --secondfilein="file:$myRawDoubleEGFile" --dirout=$doubleDir --era=Run2_2016  --scenario=pp --processName=alcaRaw --customise=L1Trigger/Configuration/customiseReEmul.L1TEventSetupForHF1x1TPs 
 else 
     echo "$myUncalZFilePath exists"
 fi
@@ -70,13 +71,13 @@ myCalWFilePath="$singleDir/EcalCalWElectron.root"
 myCalZFilePath="$doubleDir/EcalCalZElectron.root"
 if [ ! -f "$myCalWFilePath" ]; then
     echo "$myCalWFilePath doesn't exist"
-    cmsDriver.py myreco -s ALCA:EcalCalWElectron -n 100 --data --filein="file:$mySingleERecoFilePath" --conditions=$GT --secondfilein="file:$myRawSingleEFile" --dirout=$singleDir --era=Run2_2016  --scenario=pp --processName=alcaReco --customise=L1Trigger/Configuration/customiseReEmul.L1TEventSetupForHF1x1TPs 
+    cmsDriver.py myreco -s ALCA:EcalCalWElectron -n $num --data --filein="file:$mySingleERecoFilePath" --conditions=$GT --secondfilein="file:$myRawSingleEFile" --dirout=$singleDir --era=Run2_2016  --scenario=pp --processName=alcaReco --customise=L1Trigger/Configuration/customiseReEmul.L1TEventSetupForHF1x1TPs 
 else
     echo "$myCalWFilePath exists"
 fi
 if [ ! -f "$myCalZFilePath" ]; then
     echo "$myCalZFilePath doesn't exist"
-    cmsDriver.py myreco -s ALCA:EcalCalZElectron -n 100 --data --filein="file:$myDoubleEGRecoFilePath" --conditions=$GT --secondfilein="file:$myRawDoubleEGFile" --dirout=$doubleDir --era=Run2_2016  --scenario=pp --processName=alcaReco --customise=L1Trigger/Configuration/customiseReEmul.L1TEventSetupForHF1x1TPs 
+    cmsDriver.py myreco -s ALCA:EcalCalZElectron -n $num --data --filein="file:$myDoubleEGRecoFilePath" --conditions=$GT --secondfilein="file:$myRawDoubleEGFile" --dirout=$doubleDir --era=Run2_2016  --scenario=pp --processName=alcaReco --customise=L1Trigger/Configuration/customiseReEmul.L1TEventSetupForHF1x1TPs 
 else
     echo "$myCalZFilePath exists"
 fi
@@ -87,19 +88,19 @@ myRecalWFilePath="$singleDir/EcalRecalElectron.root"
 myRecalZFilePath="$doubleDir/EcalRecalElectron.root"
 if [ ! -f "$myRecalWFilePath" ]; then
     echo "$myRecalWFilePath doesn't exist"
-    cmsDriver.py myreco -s ALCA:EcalRecalElectron -n 100 --data --conditions=$GT --nThreads=4 --filein="file:$myUncalWFilePath" --dirout=$singleDir --customise_commands="process.options=cms.untracked.PSet(wantSummary=cms.untracked.bool(True))"   --process=RERECO --customise Calibration/EcalAlCaRecoProducers/customRereco.EcalRecal --era=Run2_2016  --scenario=pp --customise=L1Trigger/Configuration/customiseReEmul.L1TEventSetupForHF1x1TPs 
+    cmsDriver.py myreco -s ALCA:EcalRecalElectron -n $num --data --conditions=$GT --nThreads=4 --filein="file:$myUncalWFilePath" --dirout=$singleDir --customise_commands="process.options=cms.untracked.PSet(wantSummary=cms.untracked.bool(True))"   --process=RERECO --customise Calibration/EcalAlCaRecoProducers/customRereco.EcalRecal --era=Run2_2016  --scenario=pp --customise=L1Trigger/Configuration/customiseReEmul.L1TEventSetupForHF1x1TPs 
 else
     echo "$myRecalWFilePath exists"
 fi
 if [ ! -f "$myRecalZFilePath" ]; then
     echo "$myRecalZFilePath doesn't exist"
-    cmsDriver.py myreco -s ALCA:EcalRecalElectron -n 100 --data --conditions=$GT --nThreads=4 --filein="file:$myUncalZFilePath" --dirout=$doubleDir --customise_commands="process.options=cms.untracked.PSet(wantSummary=cms.untracked.bool(True))"   --process=RERECO --customise Calibration/EcalAlCaRecoProducers/customRereco.EcalRecal --era=Run2_2016  --scenario=pp --customise=L1Trigger/Configuration/customiseReEmul.L1TEventSetupForHF1x1TPs 
+    cmsDriver.py myreco -s ALCA:EcalRecalElectron -n $num --data --conditions=$GT --nThreads=4 --filein="file:$myUncalZFilePath" --dirout=$doubleDir --customise_commands="process.options=cms.untracked.PSet(wantSummary=cms.untracked.bool(True))"   --process=RERECO --customise Calibration/EcalAlCaRecoProducers/customRereco.EcalRecal --era=Run2_2016  --scenario=pp --customise=L1Trigger/Configuration/customiseReEmul.L1TEventSetupForHF1x1TPs 
 else
     echo "$myRecalZFilePath exists"
 fi
 
 echo "[INFO] NTuples from ALCARECO"
-#cmsRun python/alcaSkimming.py isCrab=0 skim=WSkim maxEvents=100 type=ALCARECO files="file:$myCalWFilePath" doTree=3 tagFile=config/reRecoTags/test80x.py doTreeOnly=1 
-#cmsRun python/alcaSkimming.py isCrab=0 skim=ZSkim maxEvents=100 type=ALCARECO files="file:$myCalZFilePath" doTree=3 tagFile=config/reRecoTags/test80x.py doTreeOnly=1 
+cmsRun python/alcaSkimming.py isCrab=0 skim=WSkim maxEvents=$num type=ALCARECO files="file:$myCalWFilePath" doTree=3 tagFile=config/reRecoTags/test80x.py doTreeOnly=1 
+#cmsRun python/alcaSkimming.py isCrab=0 skim=ZSkim maxEvents=$num type=ALCARECO files="file:$myCalZFilePath" doTree=3 tagFile=config/reRecoTags/test80x.py doTreeOnly=1 
 
 exit 0
